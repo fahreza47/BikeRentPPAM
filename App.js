0dfcf770e0f3d1e387d3a073429a1f18d7a6d76e
fahreza47 <117149375+fahreza47@users.noreply.github.com>
2024-06-11 20:45:51 +0700

    repo PPAM

---
+++ App.js
@@ -0,0 +1,10 @@
+import Routes from './src/routes';
+import React from 'react';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Routes />
+    </React.Fragment>
+  );
+}
